50640ee23ab0cb0dd5e6f1d0703710671148e7de
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-03-04 17:22:03 -0500

    commit tree 4ea1fab0c4b6233b873f6b76b8322f1a9cd66657

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('')
 }
 
 /* 
   Add your pseudocode here
+  Bro I got it right the first time around...
 */
 
 /*
   Add written explanation of your solution here
+  Idk man, I did it right at first...
 */
 
 // You can run `node index.js` to view these console logs
